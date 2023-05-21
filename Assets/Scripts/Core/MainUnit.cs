f89d4d53aaa5c129f7fe89e0f0703a7d686238b3
Artur <yermak01artur@gmail.com>
2023-05-21 13:23:00 +0200

    Created class for Unit

---
+++ Assets/Scripts/Core/MainUnit.cs
@@ -0,0 +1,18 @@
+using Abstractions;
+using UnityEngine;
+
+namespace Core
+{
+    public class MainUnit: MonoBehaviour, ISelectable
+    {
+
+        [SerializeField] private float _maxHealh = 200;
+        [SerializeField] private Sprite _icon;
+
+        private float _health = 200;
+
+        public float Health => _health;
+        public float MaxHealth => _maxHealh;
+        public Sprite Icon => _icon;
+    }
+}
